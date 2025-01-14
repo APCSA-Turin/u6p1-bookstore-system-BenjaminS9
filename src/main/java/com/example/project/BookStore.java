2156fe81d5e214a957142473293e3e5164a98241
BenjaminS9 <benjamins235@nycstudents.net>
2025-01-14 14:32:46 +0000

    commit

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -3,29 +3,75 @@ package com.example.project;
 public class BookStore{
 
     //requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
-
+    private Book[] books = new Book[5];
+    private User[] users = new User[10];
     //requires 1 empty constructor
-
+    public BookStore() {}
     // public getUsers(){}
-
+    public User[] getUsers() {
+        return users;
+    }
     // public setUsers(){}
-
+    public void setUsers(User[] newUsers) {
+        users = newUsers;
+    }
     // public  getBooks(){}
-
+    public Book[] getBooks() {
+        return books;
+    }
     // public void addUser(User user){} 
-
+    public void addUser(User user) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] == null) {
+                users[i] = user;
+                break;
+            }
+        }
+    }
     // public void removeUser(User user){}
-
+    public void removeUser(User user) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] == user) {
+                users[i] = null;
+            }
+        }
+        consolidateUsers();
+    }
     // public void consolidateUsers(){}
-
+    public void consolidateUsers() {
+        User[] tempUsers = new User[10];
+        int idx = 0;
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null) {
+                tempUsers[idx] = users[i];
+                idx++;
+            }
+        }
+        users = tempUsers;
+    }
     // public void addBook(Book book){}
-
+    public void addBook(Book book) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                books[i] = book;
+                break;
+            }
+        }
+    }
     // public void insertBook(Book book, int index){}
-
+    public void insertBook(Book book, int index) {
+        books[index] = book;
+    }
     // public void removeBook(Book book){}
-       
+    public void removeBook(Book book) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == book) {
+                books[i] = null;
+            }
+        }
+    }
     // public String bookStoreBookInfo(){} //you are not tested on this method but use it for debugging purposes
-
+    
     // public String bookStoreUserInfo(){} //you are not tested on this method but use it for debugging purposes
 
 }
--- src/main/java/com/example/project/IdGenerate.java
+++ src/main/java/com/example/project/IdGenerate.java
@@ -3,14 +3,22 @@ package com.example.project;
 public class IdGenerate{ //This class contains statics variable and methods, you do not initalize an object to use it.
     
     // //requires 1 private attribute String currentId. You must initialize it to 99
-
+    private static String currentId = "99";
     // //requires one empty constructor
-
+    public IdGenerate() {}
 
     // public  getCurrentId(){}
-
+    public static String getCurrentId() {
+        return currentId;
+    }
     // public  reset(){} //must reset the currentId back to 99
-
+    public static void reset() {
+        currentId = "99";
+    }
 
     // public generateID(){} //generates a new id, when called it will increment the currentId by 1.. Hint pay attention to data type of currentId
+    public static void generateID() {
+        int numId = Integer.parseInt(currentId) + 1; //I learned this from: https://stackoverflow.com/questions/5585779/how-do-i-convert-a-string-to-an-int-in-java
+        currentId = String.valueOf(numId); //I learned this from: https://stackoverflow.com/questions/5071040/java-convert-integer-to-string
+    }
 }
